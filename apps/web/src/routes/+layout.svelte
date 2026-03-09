72e07722d23a74940669a9471be306198cedba48
Jesse Hoyos <jesse@mojotech.com>
2026-03-09 19:28:57 -0400

    feat(layout): add seline analytics script

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,13 +2,24 @@
 	import { ModeWatcher } from 'mode-watcher';
     import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
-	import Header from '$lib/components/Header.svelte';
+    import Header from '$lib/components/Header.svelte';
+    import favicon from "$lib/assets/favicon.svg";
 	import '../app.css';
 
     injectSpeedInsights();
 
 	const { children } = $props();
 </script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <!-- Seline analytics -->
+  <script
+    async
+    src="https://cdn.seline.com/seline.js"
+    data-token="5d1b0e6affbaf65"
+  ></script>
+</svelte:head>
 
 <ModeWatcher />
 
